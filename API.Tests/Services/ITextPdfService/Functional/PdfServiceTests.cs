8a742fe9f2305ce7d2218ce71ead278f004a07f5
ekaterina.kurganskaya <ekaterina.kurganskaya@b-cars.ch>
2025-06-25 14:45:56 +0200

    Tests for returning fonts and adding a text

--- API.Tests/Services/ITextPdfService/Functional/PdfServiceTests.cs
+++ API.Tests/Services/ITextPdfService/Functional/PdfServiceTests.cs
@@ -3,6 +3,8 @@ using API.Services.ITextPdfService;
 using iText.Forms;
 using iText.Forms.Fields;
 using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
 using iText.Kernel.Pdf.Xobject;
 using Microsoft.AspNetCore.Http;
 using Moq;
@@ -17,7 +19,7 @@ public class PdfServiceTest
 
     public PdfServiceTest()
     {
-        _pdfService = new PdfService();
+        _pdfService = new PdfService(null);
     }
 
     [Fact(DisplayName = "ReadFields should return fields from read PDF")]
@@ -63,6 +65,22 @@ public class PdfServiceTest
 
         Assert.NotNull(pageInfo.Height);
         Assert.True(pageInfo.Height > 0);
+    }
+
+    [Fact(DisplayName = "ReadFields should return a list of all available fonts from read PDF")]
+    public void ReadFields_ShouldReturnAvailableFontsList()
+    {
+        // Arrange
+        var pdfFile = CreateSimplePdfForm(new List<FillRequest.Field>());
+
+        // Act
+        var fonts = _pdfService.ReadFields(pdfFile.Object).fonts;
+
+        // Assert
+        Assert.NotNull(fonts);
+        Assert.NotEmpty(fonts);
+        Assert.Contains("courier", fonts);//One of the fonts automatically registered in the itext library
+        Assert.Contains("courier-bold", fonts);//One of the fonts automatically registered in the itext library
     }
 
     [Fact(DisplayName = "Fill should fill fields in real PDF")]
@@ -158,7 +176,7 @@ public class PdfServiceTest
         Assert.Equal("png", img.IdentifyImageFileExtension());
     }
 
-    [Fact(DisplayName = "Fill should return an error if the scale is less than or equal to 0 or the width or height of the image after applying the scale or width and height, if specified, is greater than the width or height of the page.")]
+    [Fact(DisplayName = "Fill should return an error if the scale is less than or equal to 0, or if the image width or height after applying the scale, or the width and height if specified, is greater than the page width or height.")]
     public void Fill_ShouldThrowError_WhenIncorrectImageSize()
     {
         // Arrange
@@ -201,6 +219,132 @@ public class PdfServiceTest
 
     //Black rectangle 50x30 (.png)
     private readonly string ImageBase64String = "iVBORw0KGgoAAAANSUhEUgAAADIAAAAeCAIAAADhM9qrAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAcSURBVFhH7cExAQAAAMKg9U9tDQ8gAAAAAK7UABGyAAFALqTGAAAAAElFTkSuQmCC";
+
+    [Fact(DisplayName = "Fill should add a text in a real PDF")]
+    public void Fill_ShouldAddText()
+    {
+        // Arrange
+        var pdfFile = CreateSimplePdfForm(new List<FillRequest.Field>());
+        var firstfont = _pdfService.ReadFields(pdfFile.Object).fonts[0];
+
+        pdfFile.Object.OpenReadStream().Position = 0;
+        var textToAdd = "Test text1";
+        var fieldsToFill = new List<FillRequest.Field>{
+            new FillRequest.Field 
+            { 
+                Page = 1, 
+                Value = textToAdd,
+                TextStyle = new FillRequest.TextStyle
+                {
+                    HorizontalAlignment = FillRequest.TextHorizontalAlignment.CENTER,
+                    VerticalAlignment = FillRequest.TextVerticalAlignment.MIDDLE,
+                    Color = "#00ff00",
+                    Font = new FillRequest.FontStyle { Name = firstfont, Size = 8 }
+                }
+            }
+        };
+
+        // Act & Assert
+        var filledPdfBytes = _pdfService.Fill(pdfFile.Object, fieldsToFill);
+        Assert.NotNull(filledPdfBytes);
+
+        using var pdfDocument = new PdfDocument(new PdfReader(new MemoryStream(filledPdfBytes)));
+
+        var docText = PdfTextExtractor.GetTextFromPage(pdfDocument.GetPage(1), new SimpleTextExtractionStrategy());
+        Assert.Equal(textToAdd, docText);
+    }
+
+    [Fact(DisplayName = "Fill should return an error if the x, y coordinates, or the width or height of the text box, if specified, are greater than the width or height of the page.")]
+    public void Fill_ShouldThrowError_WhenIncorrectTextFieldPositionOrSize()
+    {
+        // Arrange
+        var pdfFile = CreateSimplePdfForm(new List<FillRequest.Field>());
+
+        var firstPage = _pdfService.ReadFields(pdfFile.Object).pages[0];
+        var pageWidth = firstPage.Width;
+        var pageHeight = firstPage.Height;
+
+        // Arrange
+        pdfFile.Object.OpenReadStream().Position = 0;
+        var fieldsToFill = new List<FillRequest.Field>{
+            new FillRequest.Field { Value = "Test text1", X = pageWidth + 50 , Y = pageHeight + 100 }
+        };
+
+        // Act & Assert
+        var exception = Assert.Throws<ArgumentException>(() => _pdfService.Fill(pdfFile.Object, fieldsToFill));
+        Assert.Contains("Invalid text coordanate X value", exception.Message);
+
+        // Arrange
+        pdfFile.Object.OpenReadStream().Position = 0;
+        fieldsToFill = new List<FillRequest.Field>{
+            new FillRequest.Field { Value = "Test text1", Y = pageHeight + 100 }
+        };
+
+        // Act & Assert
+        exception = Assert.Throws<ArgumentException>(() => _pdfService.Fill(pdfFile.Object, fieldsToFill));
+        Assert.Contains("Invalid text coordanate Y value", exception.Message);
+
+        // Arrange
+        pdfFile.Object.OpenReadStream().Position = 0;
+        fieldsToFill = new List<FillRequest.Field>{
+            new FillRequest.Field { Value = "Test text1", Width = pageWidth + 50 , Height = pageHeight + 100 }
+        };
+
+        // Act & Assert
+        exception = Assert.Throws<ArgumentException>(() => _pdfService.Fill(pdfFile.Object, fieldsToFill));
+        Assert.Contains("Invalid text width value", exception.Message);
+
+        // Arrange
+        pdfFile.Object.OpenReadStream().Position = 0;
+        fieldsToFill = new List<FillRequest.Field>{
+            new FillRequest.Field { Value = "Test text1", Height = pageHeight + 100 }
+        };
+
+        // Act & Assert
+        exception = Assert.Throws<ArgumentException>(() => _pdfService.Fill(pdfFile.Object, fieldsToFill));
+        Assert.Contains("Invalid text height value", exception.Message);
+    }
+
+    [Fact(DisplayName = "Fill should return an error if the font name is unknown.")]
+    public void Fill_ShouldThrowError_WhenUnknownFontName()
+    {
+        // Arrange
+        var pdfFile = CreateSimplePdfForm(new List<FillRequest.Field>());
+
+        var fieldsToFill = new List<FillRequest.Field>{
+            new FillRequest.Field
+            {
+                Value = "Test text1",
+                TextStyle = new FillRequest.TextStyle
+                {
+                    Font = new FillRequest.FontStyle { Name = "UnknownFont" }
+                }
+            }
+        };
+
+        // Act & Assert
+        var exception = Assert.Throws<ArgumentException>(() => _pdfService.Fill(pdfFile.Object, fieldsToFill));
+        Assert.Contains("The font with the name 'UnknownFont' not found", exception.Message);
+    }
+
+    [Fact(DisplayName = "Fill should return an error if the text color value is invalid.")]
+    public void Fill_ShouldThrowError_WhenInvalidColorValue()
+    {
+        // Arrange
+        var pdfFile = CreateSimplePdfForm(new List<FillRequest.Field>());
+
+        var fieldsToFill = new List<FillRequest.Field>{
+            new FillRequest.Field
+            {
+                Value = "Test text1",
+                TextStyle = new FillRequest.TextStyle { Color = "Unknown color" }
+            }
+        };
+
+        // Act & Assert
+        var exception = Assert.Throws<ArgumentException>(() => _pdfService.Fill(pdfFile.Object, fieldsToFill));
+        Assert.Contains("Unknown color value", exception.Message);
+    }
 
     private Mock<IFormFile> CreateSimplePdfForm(List<FillRequest.Field> fields)
     {
